db2b2b015a40e8d646a1d8f2edf9d194dff6d8e8
DWAA1660 <dwatnip123@gmail.com>
2023-07-18 13:33:50 -0600

    i cant lose this bruh

---
+++ Routes/AuthenticationHandler.py
@@ -0,0 +1,60 @@
+from flask import Blueprint, request, render_template, redirect, url_for, session
+import sys
+sys.path.append("..") 
+from scripts import *
+import json
+
+# Create a blueprint for the user routes
+user = Blueprint('user', __name__)
+
+@user.route('/register', methods=['POST', 'GET'])
+def register_user():
+    if request.method == "POST":
+        print(1)
+        data = request.form
+        email = data.get('email')
+        password = data.get('password')
+        name = data.get('username')
+
+        try:
+            response = register(email, password, name)
+            print(8)
+            try:
+                _ = json.load(response)
+                return response
+            except Exception:
+                session['email'] = email
+                return redirect(url_for('index'))
+        except Exception as e:
+            pass
+    else:
+        print(2)
+        return render_template("register.html")
+
+
+@user.route('/login', methods=['POST', 'GET'])
+def login_user():
+    if request.method == "POST":
+        print(1)
+        data = request.form
+        email = data.get('email')
+        password = data.get('password')
+        try:
+            response = login(email, password)
+            print(8)
+            try:
+                _ = json.load(response)
+                return response
+            except Exception:
+                session['email'] = email
+                return redirect(url_for('index'))
+        except Exception as e:
+            pass
+    else:
+        print(2)
+        return render_template("login.html")
+
+@user.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
