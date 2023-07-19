2b37142e6e82d17389893f2a3402c5d4210369f9
DWAA1660 <dwatnip123@gmail.com>
2023-07-19 12:16:58 -0600

    alot

--- Routes/Servers.py
+++ Routes/Servers.py
@@ -7,6 +7,9 @@ servers = Blueprint('servers', __name__)
 
 @servers.route('/')
 def servers_index():
+    if not 'email' in session:
+        return redirect(url_for('user.login_user'))
+        
     if 'pterodactyl_id' in session:
         id = session['pterodactyl_id']
     else:
@@ -14,7 +17,6 @@ def servers_index():
         session['pterodactyl_id'] = id
         
     servers = list_servers(id[0][0])
-    print(servers)
     return render_template('servers.html', servers=servers)
 
 @servers.route('/<server_id>')
@@ -23,3 +25,98 @@ def server(server_id):
     info = get_server_information(server_id)
     print(info)
     return render_template('server.html', info=info)
+
+@servers.route("/create")
+def create_server():
+    if not 'email' in session:
+        return redirect(url_for('user.login_user'))
+    nodes = get_nodes()
+    eggs = get_eggs()
+    cnx = mysql.connector.connect(
+    host=HOST,
+    user=USER,
+    password=PASSWORD,
+    database=DATABASE
+    )
+
+    cursor = cnx.cursor()
+    query = f"SELECT server_limit FROM users where email = '{session['email']}'"
+    cursor.execute(query)
+    rows = cursor.fetchone()
+    cursor.close()
+    cnx.close()
+    print(rows[0])
+    return render_template('create_server.html', eggs=eggs, nodes=nodes)
+
+@servers.route("/delete/<server_id>")
+def delete_server(server_id):
+    if not 'email' in session:
+        return redirect(url_for('user.login_user'))
+    resp = requests.get(f"{PTERODACTYL_URL}api/application/servers/{int(server_id)}", headers=HEADERS).json()
+    cnx = mysql.connector.connect(
+    host=HOST,
+    user=USER,
+    password=PASSWORD,
+    database=DATABASE
+    )
+
+    cursor = cnx.cursor()
+    query = f"SELECT pterodactyl_id FROM users where email = '{session['email']}'"
+    cursor.execute(query)
+    rows = cursor.fetchone()
+    cursor.close()
+    cnx.close()
+    print(rows[0])
+    if resp['attributes']['user'] == rows[0]:
+        resp = requests.delete(f"{PTERODACTYL_URL}api/application/servers/{int(server_id)}", headers=HEADERS)
+        return redirect(url_for('servers.servers_index'))
+    else:
+        return "You can't delete this server you dont own it!"
+
+@servers.route('/create/submit', methods=['POST'])
+def create_server_submit():
+    if not 'email' in session:
+        return redirect(url_for('user.login_user'))
+    node_id = request.form['node_id']
+    egg_id = request.form['egg_id']
+    eggs = get_eggs()
+    for egg in eggs:
+        # print(egg_id, egg['egg_id'])
+        if int(egg['egg_id']) == int(egg_id):
+            docker_image = egg['docker_image']
+            startup = egg['docker_image']
+            break
+
+    resp = requests.get(f"{PTERODACTYL_URL}api/application/nodes/{node_id}/allocations", headers=HEADERS).json()
+    for allocation in resp['data']:
+        if allocation['attributes']['assigned'] == False:
+            alloac_id = allocation['attributes']['id']
+    body = {
+    "name": request.form['name'],
+    "user": session['pterodactyl_id'][0][0],
+    "egg": egg_id,
+    "docker_image": docker_image,
+    "startup": startup,
+
+    "limits": {
+    "memory": 128,
+    "swap": 0,
+    "disk": 512,
+    "io": 500,
+    "cpu": 15
+    },
+    "feature_limits": {
+    "databases": 0,
+    "backups": 0
+    },
+    "allocation": {
+    "default": alloac_id
+    },
+    "environment": {
+        
+    }
+    }
+    
+    res = requests.post(f"{PTERODACTYL_URL}api/application/servers", headers=HEADERS, json=body)
+    print(res.json())
+    return redirect(url_for('servers.servers_index'))
