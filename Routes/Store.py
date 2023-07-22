2beb42c95000929b909fe924fdb3223ce5a5d472
DWAA1660 <dwatnip123@gmail.com>
2023-07-22 12:52:54 -0600

    added store

---
+++ Routes/Store.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, request, render_template, redirect, url_for, session, jsonify
+import sys
+sys.path.append("..") 
+from scripts import *
+from products import products
+import json
+import stripe
+stripe.api_key = STRIPE_SECRET_KEY
+store = Blueprint('store', __name__)
+active_payments = []
+
+@store.route("/")
+def storepage():
+    products_local = products
+    for product in products_local:
+        if product['price_link'] is None:
+            products_local.remove(product)
+    print(products_local)
+    return render_template("store.html", products=products_local)
+
+
+@store.route('/checkout/<price_link>', methods=['POST', 'GET'])
+def create_checkout_session(price_link: str):
+    check_session = stripe.checkout.Session.create(
+    payment_method_types=['card'],
+    line_items=[{
+        'price': price_link,
+        'quantity': 1,
+    }],
+    mode='payment',
+    success_url=YOUR_SUCCESS_URL,
+    cancel_url=YOUR_CANCEL_URL,
+    customer_email=session['email']
+    )
+    active_payments.append(check_session['id'])
+    session['price_link'] = price_link
+    session['pay_id'] = check_session['id']
+    return redirect(check_session['url'])
+
+@store.route('/success', methods=['GET'])
+def success():
+    try:
+        id = session['pay_id']
+    except KeyError:
+        return "not valid payment"
+    check_session = stripe.checkout.Session.retrieve(id)
+    if check_session is None or id not in active_payments:
+        return "not valid payment"
+    if check_session['payment_status'] =='paid':
+        print(check_session)
+        active_payments.remove(id)
+        for product in products:
+            if product['price_link'] == session['price_link']:
+                credits_to_add = product['price']
+        add_credits(check_session['customer_email'], credits_to_add)
+        return "got moneys"
+        
+    elif check_session['status'] == 'expired':
+        active_payments.remove(id)
+        return "payment link expired"
+    
+@store.route('/cancel', methods=['GET'])
+def cancel():
+    # Handle the case when the customer cancels the payment
+    return render_template('cancel.html')
--- products.py
+++ products.py
@@ -1,5 +1,6 @@
 products = [
     {
+        "price_link": None,
         "id": 0,
         "name": "Free Tier",
         "price": 0,
@@ -9,9 +10,14 @@ products = [
             "disk": 512,
             "io": 500,
             "cpu": 15
+        },
+        "product_limits": {
+            "databases": 0,
+            "backups": 0
         }
     },
     {
+        "price_link": None,
         "id": 1,
         "name": "Ls 0.5",
         "price": 50,
@@ -21,18 +27,78 @@ products = [
             "disk": 3000,
             "io": 500,
             "cpu": 100
+        },
+        "product_limits": {
+            "databases": 0,
+            "backups": 0
         }
     },
     {
+        "price_link": "price_1Mu6MrGc0pLG1vqRomBTg8iE",
         "id": 2,
         "name": "Ls 1",
-        "price": 0,
+        "price": 100,
         "limits": {
             "memory": 1024,
             "swap": 0,
             "disk": 7168,
             "io": 500,
             "cpu": 0
+        },
+        "product_limits": {
+            "databases": 1,
+            "backups": 1
         }
-    }
+    },
+    {
+        "price_link": "price_1NWRmfGc0pLG1vqRMHNsVfho",
+        "id": 3,
+        "name": "Ls 2",
+        "price": 200,
+        "limits": {
+            "memory": 2048,
+            "swap": 0,
+            "disk": 14336,
+            "io": 500,
+            "cpu": 0
+        },
+        "product_limits": {
+            "databases": 1,
+            "backups": 2
+        }
+    },
+    {
+        "price_link": "price_1NWRoTGc0pLG1vqR9JanoqFv",
+        "id": 4,
+        "name": "Ls 3",
+        "price": 300,
+        "limits": {
+            "memory": 2048,
+            "swap": 0,
+            "disk": 21504,
+            "io": 500,
+            "cpu": 0
+        },
+        "product_limits": {
+            "databases": 2,
+            "backups": 2
+        }
+    },
+    {
+        "price_link": "price_1NWRpNGc0pLG1vqRCtS4p0tW",
+        "id": 5,
+        "name": "Ls 4",
+        "price": 400,
+        "limits": {
+            "memory": 4096,
+            "swap": 0,
+            "disk": 28672,
+            "io": 500,
+            "cpu": 0
+        },
+        "product_limits": {
+            "databases": 2,
+            "backups": 3
+        }
+    },
 ]
