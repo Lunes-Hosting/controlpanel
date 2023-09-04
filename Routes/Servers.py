afdeb50d1caabf39a15be6b65f923851624db276
DWAA1660 <dwatnip123@gmail.com>
2023-09-04 13:13:53 -0600

    fixed ip

--- Routes/Servers.py
+++ Routes/Servers.py
@@ -18,6 +18,7 @@ def servers_index():
         session['pterodactyl_id'] = id
         
     update_last_seen(session['email'])
+    update_ip(session['email'], request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
     cnx = mysql.connector.connect(
         host=HOST,
         user=USER,
@@ -48,6 +49,7 @@ def server(server_id):
     info = get_server_information(server_id)
     print(info)
     update_last_seen(session['email'])
+    update_ip(session['email'], request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
     
     return render_template('server.html', info=info)
 
@@ -64,6 +66,7 @@ def create_server():
         session['pterodactyl_id'] = id
     
     update_last_seen(session['email'])
+    update_ip(session['email'], request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
     cnx = mysql.connector.connect(
         host=HOST,
         user=USER,
@@ -99,6 +102,7 @@ def delete_server(server_id):
     if not 'email' in session:
         return redirect(url_for('user.login_user'))
     update_last_seen(session['email'])
+    update_ip(session['email'], request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
     
     resp = requests.get(f"{PTERODACTYL_URL}api/application/servers/{int(server_id)}", headers=HEADERS).json()
     cnx = mysql.connector.connect(
@@ -126,6 +130,7 @@ def create_server_submit():
     if not 'email' in session:
         return redirect(url_for('user.login_user'))
     update_last_seen(session['email'])
+    update_ip(session['email'], request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
     
     node_id = request.form['node_id']
     egg_id = request.form['egg_id']
