63ca7f22ea58836ce4548d5e5c67f4705fd43b37
DWAA1660 <dwatnip123@gmail.com>
2023-09-04 13:13:53 -0600

    fixed ip

--- main.py
+++ main.py
@@ -173,8 +173,8 @@ def register_user():
         email = data.get('email')
         password = data.get('password')
         name = data.get('username')
-        ip = request.remote_addr
-
+        ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+        update_ip(session['email'], request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
         register(email, password, name, ip)
 
         # Generate a verification token
@@ -269,6 +269,7 @@ def sync_users():
 def index():
         if 'email' in session:
             update_last_seen(session['email'])
+            update_ip(session['email'], request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
             return render_template('index.html')
         else:
             return redirect(url_for('user.login_user'))
