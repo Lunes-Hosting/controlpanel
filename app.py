8a3e7be2f23acdcd4283e75b9a801a6f1d6ffd37
Josecholi <41088444+Josecholi@users.noreply.github.com>
2024-08-15 04:32:03 -0400

    idek why im adding this

--- app.py
+++ app.py
@@ -351,3 +351,4 @@ def index():
 if __name__ == '__main__':
     # Create separate processes for Flask and the Discord bot
     app.run(debug=False, host="0.0.0.0", port=1137)
+    webhook_log("dashboard has started up")
