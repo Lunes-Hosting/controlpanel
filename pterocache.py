123427b31b9e6b1482d5ca4501e6466a9b5e2735
DWAA1660 <dwatnip123@gmail.com>
2024-09-07 13:47:45 -0600

    disabled default eggs

--- pterocache.py
+++ pterocache.py
@@ -21,20 +21,22 @@ class PteroCache():
         self.update_node_cache()
     
     def update_egg_cache(self):
+        disabled_nests = [15]
         try:
             available_eggs = []
             nests = requests.get(f"{PTERODACTYL_URL}api/application/nests", headers=self.HEADERS)
 
             nests_data = nests.json()
             for nest in nests_data['data']:
-                resp = requests.get(f"{PTERODACTYL_URL}api/application/nests/{nest['attributes']['id']}/eggs", headers=self.HEADERS)
-                data = resp.json()
-                for egg in data['data']:
-                    attributes = egg['attributes']
-                    available_eggs.append(
-                        {"egg_id": attributes['id'], "name": attributes['name'], "docker_image": attributes['docker_image'],
-                        "startup": attributes['startup']}
-                    )
+                if nest['attributes']['id'] not in disabled_nests:
+                    resp = requests.get(f"{PTERODACTYL_URL}api/application/nests/{nest['attributes']['id']}/eggs", headers=self.HEADERS)
+                    data = resp.json()
+                    for egg in data['data']:
+                        attributes = egg['attributes']
+                        available_eggs.append(
+                            {"egg_id": attributes['id'], "name": attributes['name'], "docker_image": attributes['docker_image'],
+                            "startup": attributes['startup']}
+                        )
             self.egg_cache = available_eggs
             return
         except KeyError as e:
