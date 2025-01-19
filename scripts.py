a5b51dff04d83d609a3c5b5e1c05bb5a6360e030
DWAA1660 <dwatnip123@gmail.com>
2025-01-18 21:33:21 -0700

    sync users better

--- scripts.py
+++ scripts.py
@@ -143,7 +143,8 @@ def sync_users_script():
                     
                     query = ("INSERT INTO users (name, email, password, id, pterodactyl_id, credits) VALUES (%s, %s, %s, %s, %s, %s)")
                     values = (user_username, user_email, user_password, user_id, user_ptero_id, 25)
-                    db.execute_query(query, values)
+                    print(query, values)
+                    # db.execute_query(query, values)
                 except Exception as e:
                     error_message = f"Error adding user {user_email}: {str(e)}"
                     print(error_message)
