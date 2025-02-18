63e87d28cd944b94d6d834671f599b453242c417
Lennard <lanbrokhman@gmail.com>
2025-02-17 16:25:17 -0800

    ptero class

---
+++ managers/pterodactyl.py
@@ -0,0 +1,103 @@
+import requests
+from config import PTERODACTYL_ADMIN_KEY, PTERODACTYL_ADMIN_USER_KEY, PTERODACTYL_URL
+from managers.database_manager import DatabaseManager
+
+class Pterodactyl(DatabaseManager):
+    HEADERS = {
+    "Authorization": f"Bearer {PTERODACTYL_ADMIN_KEY}",
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+    }
+
+    CLIENT_HEADERS = {
+        "Authorization": f"Bearer {PTERODACTYL_ADMIN_USER_KEY}",
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    def improve_list_servers(self, pterodactyl_id: int = None) -> tuple[dict]:
+        """
+        
+        Example Response:
+        {
+            "object": "user",
+            "attributes": {
+                "id": 2,
+                "external_id": null,
+                "uuid": "4cbba6d2-923b-4630-b7ac-8b9dcf204bb0",
+                "username": "gustavo_fring",
+                "email": "frostyornot@gmail.com",
+                "first_name": "gustavo_fring",
+                "last_name": "gustavo_fring",
+                "language": "en",
+                "root_admin": true,
+                "2fa": false,
+                "created_at": "2022-09-28T20:52:34+00:00",
+                "updated_at": "2024-07-10T03:29:42+00:00",
+                "relationships": {
+                "servers": {
+                    "object": "list",
+                    "data": [
+                    {
+                        "object": "server",
+                        "attributes": {
+                        "id": 2765,
+                        "external_id": null,
+                        "uuid": "01ed4077-d73e-4988-8e62-370a77df3fe6",
+                        "identifier": "01ed4077",
+                        "name": "</h1>",
+                        "description": "",
+                        "status": null,
+                        "suspended": false,
+                        "limits": {
+                            "memory": 512,
+                            "swap": 0,
+                            "disk": 0,
+                            "io": 500,
+                            "cpu": 0,
+                            "threads": null,
+                            "oom_disabled": true
+                        },
+                        "feature_limits": {
+                            "databases": 0,
+                            "allocations": 0,
+                            "backups": 0
+                        },
+                        "user": 2,
+                        "node": 11,
+                        "allocation": 8105,
+                        "nest": 6,
+                        "egg": 16,
+                        "container": {
+                            "startup_command": "{{STARTUP_COMMAND}}",
+                            "image": "ghcr.io/parkervcp/yolks:python_3.11",
+                            "installed": 1,
+                            "environment": {
+                            "STARTUP_COMMAND": "eval git reset --hard HEAD && git checkout main && git pull && pip install -r requirements.txt -U && python3 main.py",
+                            "STARTUP": "{{STARTUP_COMMAND}}",
+                            "P_SERVER_LOCATION": "US",
+                            "P_SERVER_UUID": "01ed4077-d73e-4988-8e62-370a77df3fe6",
+                            "P_SERVER_ALLOCATION_LIMIT": 0
+                            }
+                        },
+                        "updated_at": "2024-08-31T02:42:51+00:00",
+                        "created_at": "2023-10-30T01:48:27+00:00"
+                        }
+                    }
+                    ]
+                }
+                }
+            }
+        }
+        """
+        resp = requests.get(
+            f"{PTERODACTYL_URL}api/application/users/{int(pterodactyl_id)}?include=servers", 
+            headers=self.HEADERS, 
+        timeout=60).json()
+
+        relationship = resp["attributes"]["relationships"]["servers"]["data"]
+
+        # Using list comprehension for faster filtering
+        server_list = [server for server in relationship if int(server["attributes"]["user"]) == pterodactyl_id]
+
+        return (server_list)
